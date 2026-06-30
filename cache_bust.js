c4a9ec15e1e2657a54c1909d2d7129d627085e58
FarhanMaura <farhanmaura89@gmail.com>
2026-06-30 08:49:07 +0700

    Fix script path for vercel

--- cache_bust.js
+++ cache_bust.js
@@ -2,8 +2,13 @@ const fs = require('fs');
 const path = 'public/bab-1/index.html';
 let html = fs.readFileSync(path, 'utf8');
 
-// Update script tag to bust cache
-html = html.replace(/<script src="animasi-bab1\.js(\?v=\d+)?"/g, '<script src="animasi-bab1.js?v=' + Date.now() + '"');
+// Regex yang mencari tag script animasi (baik path relatif maupun absolute)
+const scriptRegex = /<script src="(\/bab-1\/)?animasi-bab1\.js[^>]*><\/script>/g;
+
+// Ganti dengan versi absolute path yang baru
+const newScriptTag = `<script src="/bab-1/animasi-bab1.js?v=${Date.now()}"></script>`;
+
+html = html.replace(scriptRegex, newScriptTag);
 
 fs.writeFileSync(path, html);
 console.log('Cache busted in index.html');
